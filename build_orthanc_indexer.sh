3208c608c9595fcb2ae9a9509c141c4b4a8a2ded
CGDogan <126820728+CGDogan@users.noreply.github.com>
2023-08-30 10:21:18 +0100

    temp

--- build_orthanc_indexer.sh
+++ build_orthanc_indexer.sh
@@ -13,12 +13,21 @@ curl https://lsb.orthanc-server.com/plugin-indexer/mainline/libOrthancIndexer.so
 
 else
 
-apt-get -q install -y mercurial cmake make g++ patch unzip
-hg clone https://orthanc.uclouvain.be/hg/orthanc-indexer
+# TODO uncommentt
+#apt-get -q install -y mercurial cmake make g++ patch unzip
+#hg clone https://orthanc.uclouvain.be/hg/orthanc-indexer
+
+#export COLLECT_NO_DEMANGLE=1
+apt-get -q install -y git libdcmtk-dev
+git clone https://github.com/CGDogan/orthanc-indexer
+
+
 cd orthanc-indexer
 mkdir build
 cd build
 cmake .. -DSTATIC_BUILD=ON -DCMAKE_BUILD_TYPE=Release
+
+# If needed: VERBOSE=1
 make
 mv libOrthancIndexer.so.mainline ../../libOrthancIndexer.so
 cd ../..
